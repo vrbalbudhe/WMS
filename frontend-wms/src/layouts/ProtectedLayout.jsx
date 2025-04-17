deb2371de9b82bf9ed25a0d8677866ebdafc601a
vrbalbudhe <vrbalbudhe@gmail.com>
2025-04-17 20:08:38 +0530

    sidebar added

--- frontend-wms/src/layouts/ProtectedLayout.jsx
+++ frontend-wms/src/layouts/ProtectedLayout.jsx
@@ -1,18 +1,21 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Navbar from "../components/layoutComponents/Navbar";
 import Footer from "../components/layoutComponents/Footer";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import Sidebar from "../components/layoutComponents/Sidebar";
+import HomepageSidebar from "../components/homepage/HomepageSidebar";
 
 export const ProtectedLayout = () => {
   const { currentUser } = useContext(AuthContext);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   if (!currentUser) return <Navigate to="/" />;
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
 
   return (
     <div className="min-h-screen w-full flex bg-white">
-      <Sidebar />
+      <HomepageSidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <div className="flex flex-col flex-1">
         <div className="sticky top-0 z-40 w-full">
           <Navbar />
--- frontend-wms/src/layouts/PublicLayout.jsx
+++ frontend-wms/src/layouts/PublicLayout.jsx
@@ -1,21 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/layoutComponents/Navbar";
 import Footer from "../components/layoutComponents/Footer";
-import React from "react";
+import React, { useState } from "react";
+import HomepageSidebar from "../components/homepage/HomepageSidebar";
 
 export const PublicLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
   return (
-    <div className="min-h-screen w-full flex flex-col bg-white">
+    <div className="min-h-screen w-full flex flex-col justify-between bg-white">
       <div className="sticky top-0 z-40 w-full">
         <Navbar />
       </div>
-      <div className="flex-grow w-full min-h-screen flex justify-center items-start">
-        <div className="w-[95%] h-full">
-          <Outlet />
-        </div>
-      </div>
-      <div className="w-full bg-white">
-        <Footer />
+      <div className="w-[95%] h-full">
+        <HomepageSidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <Outlet />
       </div>
     </div>
   );
